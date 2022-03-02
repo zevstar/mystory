aa808c4a33aaf1841d05e4cbe728045f64537aad
Alana Lipkin <alana.lipkin@gmail.com>
2022-03-02 16:46:38 -0500

    character table from backend is able to be viewed on app

--- src/pages/Characters/index.js
+++ src/pages/Characters/index.js
@@ -1,13 +1,55 @@
-// 223 1:45
-import React from 'react';
+// 223 4:20
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Characters = () => {
-    return (
-        <div>
-           Character page 
-        </div>
-    );
-}
+	const [characters, setCharacters] = useState([]);
+
+	useEffect(() => {
+		fetchCharacters();
+	}, []);
+
+	const fetchCharacters = async () => {
+		try {
+			const response = await axios.get(
+				'http://localhost:8080/api/v1/allchildnames'
+			);
+
+			console.log(response);
+			setCharacters(response.data);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+	console.log('this is our state', characters);
+	return (
+		<table className='table table-striped'>
+			<thead>
+				<tr>
+					<th scope='col'>Child name</th>
+					<th scope='col'>Parent1</th>
+					<th scope='col'>Parent2</th>
+					<th scope='col'>Donor1</th>
+					<th scope='col'>Donor2</th>
+				</tr>
+			</thead>
+			<tbody>
+				{characters.map((character) => {
+					return (
+						<tr key={character.id}>
+							<td data-label='Child's Name>{character.childname}</td>
+							<td data-label="Parent 1">{character.parent1}</td>
+                            <td data-label="Parent 2">{character.parent2}</td>
+							<td data-label="Donor 1">{character.donor1}</td>
+							<td data-label="Donor 2">{character.donor2}</td>
+						</tr>
+					);
+				})}
+
+				
+			</tbody>
+		</table>
+	);
+};
 
 export default Characters;
-
